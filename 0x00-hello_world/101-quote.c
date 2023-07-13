1a44062bfc5cc2635a482c70d9274bacfbe5b59e
laasriy <laasriy10@gmail.com>
2023-07-13 15:47:59 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,8 @@
 *
 * Return: Always 0 (Success)
 */
-
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
