44d1b98a4e8f433cd17b04387a4c3edd808bbc90
laasriy <laasriy10@gmail.com>
2023-07-24 07:27:38 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -77,7 +77,7 @@ int _atoi(char *s)
 
 	sign = find_sign(s);
 
-	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9') && (idx_digit_starts <= _strlen(s))) /* count digits to print */
+	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9') && (idx_digit_starts <= _strlen(s))) 
 	{
 		digits_to_print += 1;
 		idx_digit_starts++;
